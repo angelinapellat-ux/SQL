a32a9d0c15f57d23afd9585e5cbe0318170cfab4
angelinapellat-ux <angelina.pellat@laplateforme.io>
2026-03-04 10:41:05 +0100

    feat : continue

--- Gestion stock/gstock.py
+++ Gestion stock/gstock.py
@@ -1,1 +1,20 @@
-import mysql.connector 
+import mysql.connector
+
+class StoreDB:
+    def __init__(self):
+        self.db = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="ditsiri",
+            database="store"
+        )
+        self.cursor = self.db.cursor()
+
+    def get_products(self):
+        self.cursor.execute("""
+            SELECT product.id, product.name, product.price, product.quantity, category.name
+            FROM product
+            JOIN category ON product.id_category = category.id
+        """)
+        return self.cursor.fetchall()
+
